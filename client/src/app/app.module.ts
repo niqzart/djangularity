3a5f78327a97920c2b3f604eb0536475af9b81f3
qzart45hi <qwert45hi@yandex.ru>
2022-01-05 15:27:00 +0300

    feat(client): all routes & pages

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,20 +1,46 @@
 import { NgModule } from "@angular/core"
 import { BrowserModule } from "@angular/platform-browser"
 
-import { AppRoutingModule } from "./app-routing.module"
 import { AppComponent } from "./app.component"
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
 import { MatToolbarModule } from "@angular/material/toolbar"
+import { RouterModule, Routes } from "@angular/router"
+import { ArticleListComponent } from "./article-list/article-list.component"
+import { ArticleDetailComponent } from "./article-detail/article-detail.component"
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component"
+
+const routes: Routes = [
+  {
+    path: "articles/:id",
+    component: ArticleDetailComponent,
+  },
+  {
+    path: "articles",
+    component: ArticleListComponent,
+  },
+  {
+    path: "",
+    redirectTo: "/articles",
+    pathMatch: "full"
+  },
+  {
+    path: "**",
+    component: PageNotFoundComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleListComponent,
+    ArticleDetailComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
