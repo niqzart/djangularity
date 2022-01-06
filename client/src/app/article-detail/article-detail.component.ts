4dd983442f9852fb1b781d59d136374d47f969c9
qzart45hi <qwert45hi@yandex.ru>
2022-01-06 04:19:59 +0300

    feat(client): article-detail page

--- client/src/app/article-detail/article-detail.component.ts
+++ client/src/app/article-detail/article-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from "@angular/core"
+import { ActivatedRoute } from "@angular/router"
+
+import { Article } from "../../data/articles"
 
 @Component({
   selector: "app-article-detail",
@@ -7,9 +10,12 @@ import { Component, OnInit } from "@angular/core";
 })
 export class ArticleDetailComponent implements OnInit {
 
-  constructor() { }
+  article: Article = { id: 1, title: "1", content: "heyuiad sgihdjaduyksgado pjisuaykdfsugaihdkfsajdy adshj ads dsa dsa dsa dfsajhdsga dsa da d ad ad sad ad sad d as d sad asd as d sad s d sad a f fa fa fg", created: "today", updated: "today" }
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const tmp = this.route.snapshot.paramMap.get("id")?.toString()
+    this.article.title = tmp ? tmp : "1"
   }
-
 }
