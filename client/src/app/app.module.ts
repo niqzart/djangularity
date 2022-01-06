c3d37b6ab6da271a2a47664edf5c6bedb693a911
qzart45hi <qwert45hi@yandex.ru>
2022-01-06 03:52:38 +0300

    feat(client): article-detail page (article & comment form)

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from "@angular/core"
 import { BrowserModule } from "@angular/platform-browser"
+import { RouterModule, Routes } from "@angular/router"
 
-import { AppComponent } from "./app.component"
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
 import { MatToolbarModule } from "@angular/material/toolbar"
-import { RouterModule, Routes } from "@angular/router"
+import { MatGridListModule } from "@angular/material/grid-list"
+import { MatCardModule } from "@angular/material/card"
+import { MatFormFieldModule } from "@angular/material/form-field"
+import { MatInputModule } from "@angular/material/input"
+import { MatButtonModule } from "@angular/material/button"
+
+import { AppComponent } from "./app.component"
 import { ArticleListComponent } from "./article-list/article-list.component"
 import { ArticleDetailComponent } from "./article-detail/article-detail.component"
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component"
-import { MatGridListModule } from "@angular/material/grid-list"
-import { MatCardModule } from "@angular/material/card"
 
 const routes: Routes = [
   {
@@ -44,6 +48,9 @@ const routes: Routes = [
     MatToolbarModule,
     MatGridListModule,
     MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
     RouterModule.forRoot(routes),
   ],
   providers: [],
--- client/src/app/article-detail/article-detail.component.ts
+++ client/src/app/article-detail/article-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from "@angular/core"
+
+import { Article } from "../../data/articles"
 
 @Component({
   selector: "app-article-detail",
@@ -6,6 +8,8 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./article-detail.component.css"]
 })
 export class ArticleDetailComponent implements OnInit {
+
+  article: Article = {id: 1, title: "1", content: "heyuiad sgihdjaduyksgado pjisuaykdfsugaihdkfsajdy adshj ads dsa dsa dsa dfsajhdsga dsa da d ad ad sad ad sad d as d sad asd as d sad s d sad a f fa fa fg", created: "today", updated: "today"}
 
   constructor() { }
 
