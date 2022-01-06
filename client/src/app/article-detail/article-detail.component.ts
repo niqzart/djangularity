8dee7e4483e97899f2b1cacb3e013c40f934c897
qzart45hi <qwert45hi@yandex.ru>
2022-01-06 04:49:10 +0300

    feat(client): comment list in article-details

--- client/src/app/article-detail/article-detail.component.ts
+++ client/src/app/article-detail/article-detail.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from "@angular/core"
 import { FormControl, FormGroup, Validators } from "@angular/forms"
 import { ActivatedRoute } from "@angular/router"
 
-import { Article } from "../../data/articles"
+import { Article, Comment } from "../../data/articles"
 
 @Component({
   selector: "app-article-detail",
@@ -18,14 +18,36 @@ export class ArticleDetailComponent implements OnInit {
     content: new FormControl("", [Validators.required, Validators.minLength(10)]),
   }, { updateOn: "submit" })
 
+  comments: Comment[] = []
+  hasNext: boolean = true
+  offset: number = 0
+  limit: number = 20  // const
+
+  fetchComments(): void {
+    this.comments.push(...[
+      { id: 1, username: "hey", content: "doiahduysadoihaduiasdhiakd", created: "today", updated: "today" },
+      { id: 1, username: "hey", content: "doiahduysadoihaduiasdhiakd", created: "today", updated: "today" },
+      { id: 1, username: "hey", content: "doiahduysadoihaduiasdhiakd", created: "today", updated: "today" },
+      { id: 1, username: "hey", content: "doiahduysadoihaduiasdhiakd", created: "today", updated: "today" },
+      { id: 1, username: "hey", content: "doiahduysadoihaduiasdhiakd", created: "today", updated: "today" },
+    ])  // request
+    this.offset = this.comments.length
+    this.hasNext = true  // request
+  }
+
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.fetchComments()
+
     const tmp = this.route.snapshot.paramMap.get("id")?.toString()
     this.article.title = tmp ? tmp : "1"
   }
 
   comment(): void {
+    // send
 
+    this.offset = 0
+    this.fetchComments()
   }
 }
